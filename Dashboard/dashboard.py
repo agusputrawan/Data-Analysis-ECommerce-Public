280c0636a6b9ba0557dfcf28ec4a4521684a44c5
Putu Agus Putrawan <agusputrawan.data@gmail.com>
2025-01-29 19:28:16 +0800

    Add Dashboard.py

---
+++ Dashboard/dashboard.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+import matplotlib.ticker as mtick
+import datetime as dt
+sns.set(style='dark')
+
